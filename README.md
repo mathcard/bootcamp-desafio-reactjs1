d6b423733b99aafd57de6eda5c04269cff947b53
mathcard <mathcardoso.94@gmail.com>
2020-10-29 23:06:21 -0300

    Finalizando desafios react 1

---
+++ README.md
@@ -0,0 +1,10 @@
+# bootcampo-desafio-reactjs1
+
+### Temas Abordados
+- Estado & Imutabilidade
+- Consultas via api
+- Consumo de testes 
+
+### Testes
+![](/rocket.png)
+
--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Desafio BootCamp RockerSeat React',
+      url: 'https://github.com/mathcard/bootcamp-desafio-reactjs1',
+      techs: ['reacjs', 'nodejs'],
+    });
+
+    const repositorie = response.data;
+    setRepositories([...repositories, repositorie])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    console.log(id);
+
+    await api.delete(`repositories/${id}`);
+    const newRepositories = (repositories.filter(repository => repository.id !== id));
+    setRepositories(newRepositories);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repositorie => 
+          <li key={repositorie.id}>{repositorie.title}
+            <button onClick={() => handleRemoveRepository(repositorie.id)}>Remover</button>
+          </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
